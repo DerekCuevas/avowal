3a6c623a7c3e80e30936d3ff07cfb80cbbebe9ee
Derek Cuevas <dccuevas2@gmail.com>
2015-07-31 19:51:22 -0700

    changed event

--- example/signup.js
+++ example/signup.js
@@ -54,7 +54,7 @@ function set_maximum_frequency(freq, fn, except) {
  */
 var signup = new Validation({
     name: 'signup',
-    on: 'change',
+    on: 'input',
     templates: {
         success: 'template.success',
         error: 'template.error'
