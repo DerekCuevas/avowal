6345c96459665effa6d8aa602ddac33ea648c8dd
Derek Cuevas <dccuevas2@gmail.com>
2015-11-09 13:22:54 -0800

    fixing examples

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,6 @@ var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
 
 gulp.task('deploy-examples', function() {
-    return gulp.src(['./examples/**/*', '!./examples/signup'])
+    return gulp.src(['./examples/**/*', '!./examples/signup/**/*'])
         .pipe(ghPages());
 });
