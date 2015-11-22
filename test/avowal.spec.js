c0ec01351cf359d84a10cb4f9baad206c15f0e24
Derek Cuevas <dccuevas2@gmail.com>
2015-11-22 13:28:01 -0800

    most recent

--- test/avowal.spec.js
+++ test/avowal.spec.js
@@ -1,7 +1,8 @@
 var expect = require('expect');
 
+// FIXME: write these tests...
 describe('Avowal', function () {
-    it('Should pass...', function () {
+    it('Should always pass.', function () {
         expect(true).toEqual(true);
     });
 });
